cd381afda8d1d63d905cc75ae141336e6a2a32ae
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-03-02 13:13:01 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,14 +4,8 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+    START_TEXT = """<b>𝐻𝐸𝑌 {}!!</b>
+<i>𝐴𝐷𝐷 𝑀𝐸 𝑇𝑂 𝐴 𝑌𝑂𝑈𝑅 𝐶𝐻𝐴𝑇 𝐺𝑅𝑂𝑈𝑃 𝐴𝑁𝐷 𝐸𝑁𝐽𝑂𝑌🏅🏅</i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
@@ -43,16 +37,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+▫️▫️
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/ATHIF_E_F_X_T_G_OFFLINE">𝐴𝑇𝐻𝐼𝐹 𝐸𝐹𝑋 ℙ𝔾 [𝗢𝗙𝗙𝗟𝗜𝗡𝗘🇦🇺]</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">𝑃𝑌𝑇𝐻𝑂𝑁 𝑃𝑌𝑅𝑂𝐺𝑅𝐴𝑀</a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/TEAM-FLUFFY/ATHIF-EFX">𝑆𝑂𝑈𝑅𝐶𝐸 𝐶𝑂𝐷𝐸</a></i>
 """
